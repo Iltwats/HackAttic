623b98ac4d702d4cb9d71a41235e66f6a32e8545
Iltwats <2012atulsharma@gmail.com>
2022-01-18 00:08:02 +0530

    mini miner complete

--- src/mini_miner/MainMiner.java
+++ src/mini_miner/MainMiner.java
@@ -11,36 +11,78 @@ import java.util.concurrent.ExecutionException;
 
 public class MainMiner {
     public static void main(String[] args) throws ExecutionException, InterruptedException, NoSuchAlgorithmException {
+        // Get response from API
         String response = new NetworkRequest().makeGetRequest();
-        //System.out.println(response);
+        // Get JSON from response
         JSONObject jsonObject = new JSONObject(response);
+        // Get difficulty from JSON
         String difficulty = jsonObject.get("difficulty").toString();
-        //System.out.println(difficulty);
+        // Get toBeHashed JSON with block key
         JSONObject secondJson = (JSONObject) jsonObject.get("block");
-        //Object obj = secondJson.get("data").toString();
-        //JSONArray jsonArray = new JSONArray(obj.toString());
-
-        //System.out.println(secondJson);
-        JSONObject sortedJson = getSortedJson(secondJson);
-        System.out.println(sortedJson);
-        int nonce = getNonce(sortedJson,difficulty);
+        // Get nonce from JSON and difficulty
+        int nonce = getNonce(secondJson,difficulty);
+        // Post nonce to API
         new NetworkRequest().makePostRequest(nonce);
 
     }
 
+    // Get Nonce from JSON
+    public static int getNonce(JSONObject json, String difficulty) throws NoSuchAlgorithmException {
+        int nonce = -1;
+        int diff = Integer.parseInt(difficulty);
+        String bits = "0";
+        // Convert difficulty to bits
+        String expected = bits.repeat((int) Math.ceil(diff / 4));
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        while (true) {
+            nonce++;
+            // update nonce in JSON
+            json.put("nonce", nonce);
+            // serialize JSON to string
+            String toBeHashed = json.toString();
+            // hash string
+            byte[] hash = digest.digest(toBeHashed.getBytes(StandardCharsets.UTF_8));
+            String encoded = bytesToHex(hash);
+            // check if hash starts with expected
+            if (encoded.startsWith(expected)) {
+                System.out.println(encoded);
+                break;
+            }
+        }
+        return nonce;
+    }
+
+    // Convert byte array to hex
+    private static String bytesToHex(byte[] hash) {
+        StringBuilder hexString = new StringBuilder(2 * hash.length);
+        for (int i = 0; i < hash.length; i++) {
+            String hex = Integer.toHexString(0xff & hash[i]);
+            if (hex.length() == 1) {
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+
+
+
+
+    // ---Below Functions has no role in the problem, its just for reference---
+    // Sort JSON by Key
     private static JSONObject getSortedJson(JSONObject secondJson) {
         JSONObject sortedJson = new JSONObject();
         JSONArray jsonArray = (JSONArray) secondJson.get("data");
-        Map<String,Integer> map = new HashMap<>();
+        Map<String, Integer> map = new HashMap<>();
         for (int i = 0; i < jsonArray.length(); i++) {
             JSONArray innerArray = (JSONArray) jsonArray.get(i);
             String key = innerArray.get(0).toString();
             int value = (int) innerArray.get(1);
-            map.put(key,value);
+            map.put(key, value);
         }
         map = sortByKey(map);
         List<List<Object>> lists = new ArrayList<>();
-        for(Map.Entry<String, Integer> entry : map.entrySet()) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
             String key = entry.getKey();
             int value = entry.getValue();
             List<Object> list = new ArrayList<>();
@@ -48,40 +90,12 @@ public class MainMiner {
             list.add(value);
             lists.add(list);
         }
-
-        sortedJson.put("data",lists);
-        sortedJson.put("nonce",secondJson.get("nonce"));
+        sortedJson.put("data", lists);
+        sortedJson.put("nonce", secondJson.get("nonce"));
         return sortedJson;
     }
-
-    public static int getNonce(JSONObject json, String difficulty) throws NoSuchAlgorithmException {
-        int nonce = -1;
-        int diff = Integer.parseInt(difficulty);
-        String bits = "0";
-        String expected = bits.repeat((int) Math.ceil(diff/4));
-
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-         while (true) {
-             nonce++;
-             json.put("nonce",nonce);
-             String toBeHashed = json.toString();
-             toBeHashed = toBeHashed.replaceAll(",","");
-             toBeHashed = toBeHashed.replaceAll(":","");
-             toBeHashed = toBeHashed.replaceAll(" ","");
-             System.out.println(toBeHashed);
-             byte[] hash = digest.digest(toBeHashed.getBytes(StandardCharsets.UTF_8));
-             String encoded = Base64.getEncoder().encodeToString(hash);
-             if(encoded.startsWith(expected)){
-                 System.out.println(encoded);
-                 break;
-             }
-        }
-         return nonce;
-    }
-
-    // Function to sort map by Key
-    public static HashMap<String, Integer> sortByKey(Map<String, Integer> map)
-    {
+    // Sort map by Key
+    public static HashMap<String, Integer> sortByKey(Map<String, Integer> map) {
         // Copy all data from hashMap into TreeMap
         TreeMap<String, Integer> sorted = new TreeMap<>(map);
         return new HashMap<>(sorted);
